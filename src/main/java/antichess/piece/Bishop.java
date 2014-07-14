2942d4cdf2f70304f28a9ddb4bb9700b3d320432
Knight Fu <knight.w.fu@gmail.com>
2014-07-14 15:52:02 -0400

    updated Bishop's isMoveValid method

--- src/main/java/antichess/piece/Bishop.java
+++ src/main/java/antichess/piece/Bishop.java
@@ -18,24 +18,52 @@ public class Bishop extends Piece {
       this.color = color;
    }
 
-   public boolean isMoveValid
-        ( Location location, Board board )
-   {
-      return false;
+   public boolean isMoveValid( Location location, Board board ) {
+      if( !this.isLocationReachable( location )) {
+          return false;
+      }
+
+      Location currentLocation = this.getLocation();
+
+      if( location == currentLocation){
+         return true;
+      }
+
+      int deltaX = location.getX() - currentLocation.getX(),
+          deltaY = location.getY() - currentLocation.getY(),
+          xStep = ( deltaX > 0 )? 1 : -1,
+          yStep = ( deltaY > 0 )? 1 : -1;
+
+      for( int i = 1; i < Math.abs(deltaX); i++ ){
+         Location intermediateLocation = new Location( xStep * i, yStep * i );
+         if( !board.isLocationEmpty( intermediateLocation ) ){
+             return false;
+         }
+      }
+
+      if( !board.isLocationEmpty( location ) &&
+          ( board.getPieceAt( location ).getColor() == this.getColor() ) ){
+         return false;
+      }
+
+      return true;
    }
 
-   public List<Location> getValidLocations
-        ( Board board )
+   private boolean isLocationReachable( Location location ){
+      Location currentLocation = this.getLocation();
+
+      return Math.abs(location.getX() - currentLocation.getX()) ==
+             Math.abs(location.getY() - currentLocation.getY());
+   }
+
+   private List<Location> getValid( Board board ){
+      List<Location> locations = new ArrayList<Location>();
+      return locations;
+   }
+
+   public List<Location> getValidLocations( Board board )
    {
       List<Location> locations = new ArrayList<Location>();
-      // based on the location, find all diagonals
-      // and eliminate those which cannot be reached
-      boolean NE = false,
-              NW = false,
-              SE = false,
-              SW = false;
-
-
 
       return locations;
    }
