8f52920eb417884829df0a781c8b04b847331a14
Knight Fu <knight.w.fu@gmail.com>
2014-07-14 11:25:50 -0400

    corrected the method isMoveValid for Pawn to account for the fact that Black pieces move "backwards"; added private methods that clean up the code in determining how a pawn moves based on its color; closes #8

--- src/main/java/antichess/piece/Pawn.java
+++ src/main/java/antichess/piece/Pawn.java
@@ -13,6 +13,35 @@ import java.util.List;
 import antichess.*;
 
 public class Pawn extends Piece {
+   private boolean isAtStartingLocation(){
+       return this.getLocation().getY() ==
+               ( ( this.getColor() == Color.WHITE )? 2 : 7 );
+   }
+
+   private Location getTwoForward(){
+       return new Location(
+               this.getLocation().getX(),
+               this.getLocation().getY() + ((this.getColor() == Color.WHITE)? 2 : -2 ));
+   }
+
+   private Location getForward(){
+       return new Location(
+               this.getLocation().getX(),
+               this.getLocation().getY() + ((this.getColor() == Color.WHITE)? 1 : -1 ));
+   }
+
+   private Location getLeftDiagonal(){
+      return new Location(
+               this.getLocation().getX() + ((this.getColor() == Color.WHITE)? -1 : 1),
+               this.getLocation().getY() + ((this.getColor() == Color.WHITE)? 1 : -1 ));
+   }
+
+   private Location getRightDiagonal(){
+       return new Location(
+               this.getLocation().getX() + ((this.getColor() == Color.WHITE)? 1 : -1),
+               this.getLocation().getY() + ((this.getColor() == Color.WHITE)? 1 : -1 ));
+   }
+
    /**
     *
     * @param color
@@ -39,7 +68,13 @@ public class Pawn extends Piece {
                                Board board ){
       Location currentLocation = this.getLocation();
       int deltaX = location.getX() - currentLocation.getX();
-      int deltaY = location.getY() - currentLocation.getY();
+
+      // Note: we want to keep the deltaY positive, so if the
+      // piece is black, we want to reverse the coordinate
+      // system
+      int deltaY = ( this.getColor() == Color.WHITE)?
+              location.getY() - currentLocation.getY() :
+              currentLocation.getY() - location.getY();
 
       if( deltaY < 0 || deltaY > 2 || Math.abs(deltaX) > 1 ){
          return false;
@@ -56,14 +91,41 @@ public class Pawn extends Piece {
       }
 
       if( deltaY == 2)
-         return currentLocation.getY() ==
-                 ((this.getColor() == Color.WHITE)? 2 : 7);
+      {
+         Location forwardLocation = this.getForward();
+
+         return this.isAtStartingLocation() &&
+                board.isLocationEmpty( this.getForward() ) &&
+                board.isLocationEmpty( location );
+      }
 
       return board.isLocationEmpty(location);
    }
 
    public List<Location> getValidLocations( Board board ) {
       List<Location> validLocations = new ArrayList<Location>();
+      Location currentLocation = this.getLocation(),
+               forwardLocation = this.getForward();
+
+      if( board.isLocationEmpty( forwardLocation )){
+         validLocations.add( forwardLocation );
+
+         Location twoForwardLocation = this.getTwoForward();
+         if( this.isAtStartingLocation() &&
+             board.isLocationEmpty( twoForwardLocation ) ){
+            validLocations.add( twoForwardLocation );
+         }
+      }
+
+      Location leftDiagonal = this.getLeftDiagonal();
+      if( !board.isLocationEmpty( leftDiagonal ) ){
+         validLocations.add( leftDiagonal );
+      }
+
+      Location rightDiagonal = this.getRightDiagonal();
+      if( !board.isLocationEmpty( rightDiagonal )){
+         validLocations.add( rightDiagonal );
+      }
 
       return validLocations;
    }
