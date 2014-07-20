3ce6877e9223e62594ecd236fa6fd8b6ad2854d9
Knight Fu <knight.w.fu@gmail.com>
2014-07-19 23:25:27 -0400

    added some basic methods for Player

--- src/main/java/Antichess/Player.java
+++ src/main/java/Antichess/Player.java
@@ -1,21 +1,63 @@
 package Antichess;
 
-/**
- * Created with IntelliJ IDEA.
- * User: evilwire
- * Date: 7/3/14
- * Time: 6:13 PM
- * To change this template use File | Settings | File Templates.
- */
 import java.util.ArrayList;
+import java.util.List;
 import Antichess.Piece.Piece;
+import Antichess.Piece.Pawn;
 
+/**
+ * Player - class for maintaining the pieces in play
+ *
+ * @author Knight W. Fu
+ */
 public class Player {
    private Color color;
-   private ArrayList<Piece> pieces;
+   private List<Piece> pieces;
+   private List<Piece> capturedPieces;
+   private List<Piece> activePieces;
 
-   public Player( Color color, ArrayList<Piece> pieces ) {
+   public Player( Color color, List<Piece> pieces ) {
       this.color = color;
       this.pieces = pieces;
+      this.capturedPieces = new ArrayList<Piece>();
+      this.activePieces = pieces;
+   }
+
+   public List<Piece> getActivePieces(){
+      return ( List<Piece> )
+        ( ( ArrayList<Piece> ) this.activePieces).clone();
+   }
+
+   public List<Piece> getCapturedPieces(){
+       return ( List<Piece> )
+         ( ( ArrayList<Piece> ) this.capturedPieces).clone();
+   }
+
+   public List<Piece> getPieces(){
+       return ( List<Piece> )
+         ( ( ArrayList<Piece> ) this.pieces).clone();
+   }
+
+   public void removePiece( Piece piece ){
+      int indexOfPiece =
+        this.activePieces.indexOf( piece );
+
+      // if the piece is not found, don't do anything
+      if( indexOfPiece < 0 ) return;
+
+      this.activePieces.remove( indexOfPiece );
+      this.capturedPieces.add( piece );
+   }
+
+   public void elevatePawn( Pawn pawn, Piece piece ){
+       int indexOfPawn =
+        this.activePieces.indexOf( pawn );
+
+      // if the piece is not found, don't do anything
+      if( indexOfPawn < 0 ) return;
+
+      this.pieces.add( piece );
+      this.activePieces.remove( indexOfPawn );
+      this.capturedPieces.add( indexOfPawn, piece );
    }
 }
